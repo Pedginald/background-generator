bb882f5de6b685478d8d14996098cb834b631487
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-03-24 08:51:27 -0400

    Adding query selectors and event listeners

---
+++ script.js
@@ -0,0 +1,12 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
+color1.addEventListener("input", function() {
+    
+})
+
+color2.addEventListener("input", function() {
+
+})
